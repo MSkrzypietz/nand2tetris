05abe51fe0615f566fb4e7967ab075e1262044e3
MSkrzypietz <michael.skr97@gmail.com>
2022-07-18 19:54:04 +0200

    add proper comment and string constant handling

--- projects/10/syntaxlyzer/pkg/tokenizer/tokenizer.go
+++ projects/10/syntaxlyzer/pkg/tokenizer/tokenizer.go
@@ -14,41 +14,64 @@ type Tokenizer struct {
 func New(filename string) *Tokenizer {
 	return &Tokenizer{
 		tokens:         readTokens(filename),
-		currTokenIndex: 0,
+		currTokenIndex: -1,
 	}
 }
 
 func readTokens(filename string) []string {
+	fileData := readFileDatatWithoutSingleLineComments(filename)
+	fileData = removeMultiLineComments(fileData)
+
+	var tokens []string
+	var token string
+	for i := 0; i < len(fileData); i++ {
+		char := string(fileData[i])
+		if char == " " {
+			tokens = addNonEmptyToken(tokens, token)
+			token = ""
+		} else if isSymbol(char) {
+			tokens = addNonEmptyToken(tokens, token)
+			tokens = append(tokens, char)
+			token = ""
+		} else if char == "\"" {
+			tokens = addNonEmptyToken(tokens, token)
+			closingIndex := strings.Index(string(fileData[i+1:]), "\"")
+			println(i, closingIndex)
+			println(string(fileData[i+1 : i+20]))
+			tokens = append(tokens, fileData[i+1:i+1+closingIndex])
+			i += closingIndex + 1
+			token = ""
+		} else {
+			token += char
+		}
+	}
+	tokens = addNonEmptyToken(tokens, token)
+	return tokens
+}
+
+func readFileDatatWithoutSingleLineComments(filename string) string {
 	file, _ := os.Open(filename)
 	defer file.Close()
 
-	var tokens []string
+	fileData := ""
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line, _, _ := strings.Cut(scanner.Text(), "//")
 		trimmedLine := strings.TrimSpace(line)
-		if trimmedLine == "" {
-			continue
+		if trimmedLine != "" {
+			fileData += trimmedLine
 		}
-
-		token := ""
-		for i := 0; i < len(trimmedLine)-1; i++ {
-			char := string(trimmedLine[i])
-			if char == " " {
-				tokens = addNonEmptyToken(tokens, token)
-				token = ""
-				continue
-			} else if isSymbol(char) {
-				tokens = addNonEmptyToken(tokens, token)
-				tokens = append(tokens, char)
-				token = ""
-				continue
-			}
-			token += char
-		}
-		tokens = addNonEmptyToken(tokens, token)
 	}
-	return tokens
+	return fileData
+}
+
+func removeMultiLineComments(fileData string) string {
+	beforeCommentText, _, foundComment := strings.Cut(fileData, "/*")
+	if foundComment {
+		_, afterCommentText, _ := strings.Cut(fileData, "*/")
+		return beforeCommentText + removeMultiLineComments(afterCommentText)
+	}
+	return beforeCommentText
 }
 
 func addNonEmptyToken(tokens []string, token string) []string {
