be4703113e9045f407c0f034c8d2c9d93545c441
MSkrzypietz <michael.skr97@gmail.com>
2022-07-22 22:49:07 +0200

    add directory input support

--- projects/10/syntaxlyzer/cmd/syntaxlyzer/main.go
+++ projects/10/syntaxlyzer/cmd/syntaxlyzer/main.go
@@ -1,29 +1,46 @@
 package main
 
 import (
+	"io/fs"
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"syntaxlyzer/pkg/tokenizer"
 )
 
 func main() {
-	filePath := os.Args[1]
+	inputPath := path.Clean(os.Args[1])
 
-	filename := path.Base(filePath)
-	tokenOutputFilename := filename[:strings.LastIndex(filename, ".")] + "T.xml"
-	os.Mkdir("out", os.ModePerm)
-	outputFile, err := os.Create(path.Join("out", tokenOutputFilename))
-	if err != nil {
-		log.Fatal(err)
+	var filePaths []string
+	inputPathStats, _ := os.Stat(inputPath)
+	if inputPathStats.IsDir() {
+		filepath.WalkDir(inputPath, func(filePath string, d fs.DirEntry, err error) error {
+			if path.Ext(filePath) == ".jack" {
+				filePaths = append(filePaths, filePath)
+			}
+			return nil
+		})
+	} else {
+		filePaths = append(filePaths, inputPath)
 	}
 
-	writeTokenOutput(filePath, outputFile)
+	os.Mkdir("out", os.ModePerm)
+	for _, filePath := range filePaths {
+		filename := path.Base(filePath)
+		tokenOutputFilename := filename[:strings.LastIndex(filename, ".")] + "T.xml"
+		outputFile, err := os.Create(path.Join("out", tokenOutputFilename))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	outputFile.Sync()
-	outputFile.Close()
+		writeTokenOutput(filePath, outputFile)
+
+		outputFile.Sync()
+		outputFile.Close()
+	}
 }
 
 func writeTokenOutput(inputFilePath string, outputFile *os.File) {
